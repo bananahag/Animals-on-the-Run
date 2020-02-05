9cdcc96b30aa0b6a9dc50832551b1f54ffe512cf
Albin Dahlheim <albin.dahlheim>
2020-02-05 09:29:03 +0100

    Cage shakes when interacted with

--- Assets/Scripts/Cage.cs
+++ Assets/Scripts/Cage.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class Cage : MonoBehaviour
 {
+    AudioSource audioSource;
+
+    public AudioClip openSFX;
+    public float shakeSpeed = 50.0f;
+    public float shakeDuration = 0.5f;
+
+    bool shaking;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (shaking)
+        {
+            float AngleAmount = (Mathf.Cos(Time.time * shakeSpeed) * 180) / Mathf.PI * 0.5f;
+            AngleAmount = Mathf.Clamp(AngleAmount, -15, 15);
+            transform.localRotation = Quaternion.Euler(0, 0, AngleAmount);
+        }
     }
 
-
-    void OnTriggerEnter2D(Collider2D other)
+    public void Open()
     {
-        
+        //audioSource.PlayOneShot(openSFX);
+        StartCoroutine(Shake());
+    }
+
+    IEnumerator Shake()
+    {
+        shaking = true;
+        yield return new WaitForSeconds(shakeDuration);
+        shaking = false;
+        transform.localRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
     }
 }
