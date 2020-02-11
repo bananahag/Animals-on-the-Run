abcc8520e9d45d0d84cb8aee5c2259c53ca95edc
William gullbrandson <wille.wow@live.se>
2020-02-11 10:24:47 +0100

    asdas

---
+++ Assets/Scripts/MovingScale.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovingScale : MonoBehaviour
+{
+    
+    public GameObject OtherScale;
+    public GameObject AnchorPointTop;
+    public GameObject AnchorPoint;
+    private Vector3 otherScaleVelocityRB;
+    public float goingUp;
+    public float goingDown;
+    public int amountofboxes = 0;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        otherScaleVelocityRB = OtherScale.gameObject.GetComponent<Rigidbody2D>().velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (amountofboxes == 0)
+        {
+            transform.position = AnchorPoint.transform.position;
+        }
+        else
+        {
+
+        }
+        
+    }
+}
