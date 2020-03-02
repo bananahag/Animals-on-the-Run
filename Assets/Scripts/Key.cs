1f11b18b96f4fa9d94e2fcfe19cbc6ee7614a697
Albin Dahlheim <albin.dahlheim>
2020-03-02 15:31:33 +0100

    Monkey drops eel, player no longer moves when the text box is up and a locked door has been added!

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [Tooltip("The door the key opens.")]
+    public GameObject lockedDoor;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Monkey")
+        {
+            lockedDoor.GetComponent<LockedDoor>().KeyCollected();
+            Destroy(gameObject);
+        }
+    }
+}
