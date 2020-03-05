5b2aaca5b3313ddde0ebff361149cb99c82956be
William gullbrandson <wille.wow@live.se>
2020-03-05 15:49:04 +0100

    NPC Fix

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -4,17 +4,67 @@ using UnityEngine;
 
 public class Human : MonoBehaviour
 {
-    public float speed = 3;
+    public float travelTime = 3;
+    private float fraction;
     public bool moving;
+    private bool movingLeft = true;
+   
     public bool charmed;
     private Rigidbody2D rb;
     public BoxCollider2D box;
+    public float maxDistanceRight;
+    public float maxDistanceLeft;
+    Vector3 distanceRight;
+    Vector3 distanceLeft;
+    Vector3 moveSpeed;
+    Vector3 oldpos;
+    Vector3 gizmopos;
+
+  
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        box = GameObject.Find("HumanCollider").GetComponent<BoxCollider2D>();
+        box = GetComponentInChildren<BoxCollider2D>();
         charmed = false;
+        distanceLeft = new Vector3(-maxDistanceLeft, 0, 0) + transform.position;
+        distanceRight = new Vector3(maxDistanceRight, 0, 0) + transform.position;
+        oldpos = transform.position;
+        gizmopos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (moving)
+         {
+            if (movingLeft)
+            {
+                fraction += Time.deltaTime / travelTime;
+                rb.MovePosition(Vector2.Lerp(oldpos, distanceLeft, fraction));
+                if (transform.position.x == distanceLeft.x)
+                {
+                    print("moving right");
+                    fraction = 0;
+                    oldpos = transform.position;
+                    movingLeft = !movingLeft;
+                }
+            }
+            else 
+            {
+                fraction += Time.deltaTime / travelTime;
+                rb.MovePosition(Vector2.Lerp(oldpos, distanceRight, fraction));
+                if (transform.position.x == distanceRight.x)
+                {
+                    print("moving left");
+                    fraction = 0;
+                    oldpos = transform.position;
+                    movingLeft = !movingLeft;
+                }
+            }
+
+         }
+
+       
     }
 
     void Update()
@@ -22,21 +72,23 @@ public class Human : MonoBehaviour
         if (charmed)
         {
             moving = false;
-            rb.isKinematic = true;
             box.enabled = false;
-            rb.velocity = Vector3.zero;
+            
 
         }
         else
         {
-            rb.isKinematic = false;
             box.enabled = true;
             moving = true;
         }
 
-        if (moving)
-        {
-            transform.position += -transform.right * speed * Time.deltaTime;
-        }
+        
+       
+    }
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector3(-maxDistanceLeft, 0, 0) + gizmopos, new Vector3(maxDistanceRight, 0, 0) + gizmopos);
     }
 }
