79c58c4797121000053bb5e2643d52554b6d9260
Alexander <alexander@familjenandersson.se>
2020-03-11 15:43:53 +0100

    fågelfix

---
+++ Assets/Scripts/everythingandnothing/BirdScore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BirdScore : MonoBehaviour
+{
+    public Text BirdCountText;
+    public int birdCountScore = 0;
+    void Update()
+    {
+        BirdCountText.text = birdCountScore.ToString();
+    }
+}
