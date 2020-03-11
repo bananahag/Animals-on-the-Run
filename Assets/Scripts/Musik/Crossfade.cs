c1d431f2c42997ca39b81447b1a94d8be6313919
Carl <carlwilliam.olander@telia.com>
2020-03-11 14:57:54 +0100

    ljud 2

---
+++ Assets/Scripts/Musik/Crossfade.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Crossfade : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    bool fading;
+    public float Part1Duration;
+    public float Part2Duration;
+    public float Part3Duration;
+    public void CrossfadeGroups(int list)
+    {
+        if (!fading)
+        {
+            StartCoroutine(Crossfades(list));
+            
+        }
+    }
+
+    IEnumerator Crossfades(int list)
+    {
+        fading = true;
+        float currentTime = 0;
+        if (list == 0)
+        {
+            while (currentTime <= Part1Duration)
+            {
+                currentTime += Time.deltaTime;
+
+                mixer.SetFloat("MusicPart1", Mathf.Log10(Mathf.Lerp(1, 0.0001f, currentTime / Part1Duration)) * 20);
+                mixer.SetFloat("MusicPart2", Mathf.Log10(Mathf.Lerp(0.0001f, 1, currentTime / Part1Duration)) * 20);
+
+                yield return null;
+            }
+        }
+        else if (list == 1)
+        {
+            while (currentTime <= Part2Duration)
+            {
+                currentTime += Time.deltaTime;
+
+                mixer.SetFloat("MusicPart2", Mathf.Log10(Mathf.Lerp(1, 0.0001f, currentTime / Part2Duration)) * 20);
+                mixer.SetFloat("MusicPart3", Mathf.Log10(Mathf.Lerp(0.0001f, 1, currentTime / Part2Duration)) * 20);
+
+                yield return null;
+            }
+        }
+        else
+        {
+            while (currentTime <= Part3Duration)
+            {
+                currentTime += Time.deltaTime;
+
+                mixer.SetFloat("MusicPart3", Mathf.Log10(Mathf.Lerp(1, 0.0001f, currentTime / Part3Duration)) * 20);
+                mixer.SetFloat("MusicPart1", Mathf.Log10(Mathf.Lerp(0.0001f, 1, currentTime / Part3Duration)) * 20);
+
+                yield return null;
+            }
+        }
+        
+
+        fading = false;
+
+    }
+}
---
+++ Assets/Scripts/Musik/SnapshotSwitch_L.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(BoxCollider))]
+public class SnapshotSwitch_L : MonoBehaviour
+{
+    public AudioMixerSnapshot snapshotToPlay;
+    public float snapshotFadetime;
+    public Crossfade mCrossfade;
+    public float duration = 1.0f;
+    private int list = 0;
+    /*  private void OnTriggerEnter()
+      {
+          if (snapshotToPlay != null)
+              snapshotToPlay.TransitionTo(snapshotFadetime);
+          BoxCollider col = GetComponent<BoxCollider>();
+          col.enabled = false;
+      }*/
+    void Start()
+    {
+        
+    }
+    public void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            
+            if (list > 2)
+            {
+                list = 0;
+            }
+            mCrossfade.CrossfadeGroups(list);
+            list++;
+        }
+
+
+    }
+}
