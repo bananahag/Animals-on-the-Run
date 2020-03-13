c3354944de20a3401bca32cf2a1f07c38a5d4cbf
Albin Dahlheim <albin.dahlheim>
2020-03-13 14:25:33 +0100

    stuts

---
+++ Assets/Scripts/moving objects/MoveToTheRight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToTheRight : MonoBehaviour
+{
+
+    public float travelSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1 * travelSpeed, 0.0f);
+    }
+}
