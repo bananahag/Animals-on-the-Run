d8937f20de0e2536c34f0e429488ba6a890c4057
Carl <carlwilliam.olander@telia.com>
2020-03-18 15:06:29 +0100

    IndustriAmbiance

---
+++ Assets/Scripts/Musik/AudioAmbienceFiller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioAmbienceFiller : MonoBehaviour
+{
+	public float minTime;
+	public float maxTime;
+	public Object prefabToSpawn;
+	public float borderX;
+	public float borderZ;
+
+	void Awake()
+	{
+		StartCoroutine (SpawnPrefabs ());
+	}
+
+	IEnumerator SpawnPrefabs()
+	{
+		yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+		Vector3 pos = new Vector3 (Random.Range (-borderX, borderX), 0f, Random.Range (-borderZ, borderZ));
+		pos = pos + transform.position;
+		Instantiate (prefabToSpawn, pos, transform.rotation);
+		StartCoroutine (SpawnPrefabs());
+	}
+}
